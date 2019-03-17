024de66083c384a15b1ff31d8a80a3c58db6dbcc
benjah1 <benjaminhuang1@gmail.com>
2019-03-17 00:16:49 -0400

    update __main__

--- awsanalysis/__main__.py
+++ awsanalysis/__main__.py
@@ -1,9 +1,9 @@
-import sys
-import logging
-import os
-from .c_aws_analysis import CAwsAnalysis
-
 if __name__ == "__main__":
+    import sys
+    import logging
+    import os
+    from .c_aws_analysis import CAwsAnalysis
+
     LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
     logging.basicConfig(level=LOGLEVEL)
     awsAnalyzer = CAwsAnalysis()
